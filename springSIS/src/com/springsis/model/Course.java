3d4d10b4b2318b6e4fe2f1f1b1f81b3c21dae23e
Shree Murugesan <smurugesan@brontecollege.ca>
2020-03-17 08:48:57 -0400

    added Attendance and Course fields/getters/setters

--- springSIS/src/com/springsis/model/Course.java
+++ springSIS/src/com/springsis/model/Course.java
@@ -1,5 +1,56 @@
 package com.springsis.model;
 
 public class Course {
-
+	
+	private int id;
+	private String title;
+	private String code;
+	private String teacher;
+	private double credits;
+	private String description;
+	private String students;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getTeacher() {
+		return teacher;
+	}
+	public void setTeacher(String teacher) {
+		this.teacher = teacher;
+	}
+	public double getCredits() {
+		return credits;
+	}
+	public void setCredits(double credits) {
+		this.credits = credits;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getStudents() {
+		return students;
+	}
+	public void setStudents(String students) {
+		this.students = students;
+	}
 }
